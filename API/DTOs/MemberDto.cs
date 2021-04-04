c35cdcefeb4d7f6c26be0e5318722f44f3e08e72
LadyMisfortunate <80325540+LadyMisfortunate@users.noreply.github.com>
2021-04-04 14:34:44 +0200

    add user detail page in client

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -27,7 +27,7 @@ namespace API.DTOs
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public ICollection<PhotoDto> Photos { get; set; }
+        public ICollection<PhotoDto> Photos { get; set; } = new List<PhotoDto>();
 
     }
 }
